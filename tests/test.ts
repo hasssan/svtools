672ce0e336f0192134b258d09cf407cab0bdf95e
A. Hassan <git@hassan.web.id>
2024-05-08 21:55:51 +0700

    test: add integration test

--- tests/test.ts
+++ tests/test.ts
@@ -2,5 +2,12 @@ import { expect, test } from '@playwright/test';
 
 test('index page has expected h1', async ({ page }) => {
 	await page.goto('/');
-	await expect(page.getByRole('heading', { name: 'Welcome to SvelteKit' })).toBeVisible();
+	await expect(page.getByRole('heading', { name: 'Stardew Valley Tools' })).toBeVisible();
+});
+
+test('upload save file successfully', async ({ page }) => {
+	await page.goto('/');
+	const input = await page.$('input[type=file]');
+	await input?.setInputFiles('./fixtures/Cargo');
+	await expect(page.getByTestId('player-name')).toHaveText('Name: Camto');
 });
